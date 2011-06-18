d1e6f28597da9726ba775b26f9cacdeccc391e73
Gregor Martynus <gregor@martynus.net>
2011-06-18 12:31:17 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PieceOfMind::Application.config.secret_token = '30a54c361762b34ad4f9662fef0c99fa880a240f5a39c02daddb6f9584db9da8dadac3a13bac65b38f468a2846fb6051251f3be7a32cef67bddb05d08867d37c'
